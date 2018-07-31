35abafafae146ea2efd3926fe13909aa148d0aa6
Lucas <rydberg@slu.edu>
2018-07-31 14:46:05 -0500

    create account works

--- audioRec/CreateAccountViewController.swift
+++ audioRec/CreateAccountViewController.swift
@@ -12,55 +12,49 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
     
-    /*
-    
     //Defined a constant that holds the URL for our web service
     //This is a test account
-    let URL_USER_REGISTER = "http://192.168.1.105/SimplifiediOS/v1/register.php"
-
+    let URL_USER_REGISTER = "http://totem-env.qqkpcqqjfi.us-east-1.elasticbeanstalk.com"
+    var token : String = ""
+    let preferences = UserDefaults.standard
+    
+    
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var repeatPassword: UITextField!
     @IBOutlet weak var emailAddress: UITextField!
-    
     @IBOutlet weak var labelMessage: UILabel!    
     
     @IBAction func registerButton(_ sender: Any) {
-        
-        //creating parameters for the post request
-        let parameters: Parameters=[
-            "username":username.text!,
-            "password":password.text!,
-            "email":emailAddress.text!,
-        ]
-        
-        //Sending http post request
-        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON
-            {
-                //I wasnn't sure if you had something in mind for a sucess message for testing. We can skip this once you test
-                response in
-                //printing response
-                print(response)
-                
-                //getting the json value from the server
-                if let result = response.result.value {
-                    
-                    //converting it as NSDictionary
-                    let jsonData = result as! NSDictionary
-                    
-                    //displaying the message in label
-                    //Again.... we probaby would just go strait to the feed or profile
-                    self.labelMessage.text = jsonData.value(forKey: "message") as! String?
-                }
+      
+        if(password.isEqual(repeatPassword)){
+            let dbManager = DatabaseManager()
+            
+            /*
+             * Expected Data Input:
+             *  variable = {"User":[{"username":"Username","password":"hi","email":"Email Address"}]}
+             */
+            
+            let variable = "{\"User\":[{\"username\":\" " + username.text! + "\",\"password\":\" " + password.text! + "\",\"email\":\" " + emailAddress.text! + "\"}]}"
+            
+            print("-----------------response from dataPost-----------------------")
+            print(dbManager.dataPost(endpoint: "api/user", data: variable))
+        } else{
+            labelMessage.text = "Passwords don't match"
         }
+        
     }
-
-    
    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // get token from preferences
+        if preferences.value(forKey: "tokenKey") == nil {
+            //  Doesn't exist
+        } else {
+            self.token = preferences.value(forKey: "tokenKey") as! String
+        }
+        
     }
-*/
  
 }
