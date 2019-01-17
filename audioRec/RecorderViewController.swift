921eae4883f074a4c97b995ba624b275b17315d0
LucasRydberg <rydberg@slu.edu>
2019-01-16 19:10:14 -0600

    move textfield with keyboard

--- audioRec/RecorderViewController.swift
+++ audioRec/RecorderViewController.swift
@@ -17,6 +17,7 @@ class RecorderViewController: UIViewController, AVAudioRecorderDelegate, UITextF
     @IBOutlet weak var recorderNavBtn: UIBarButtonItem!
     @IBOutlet weak var profileNavBtn: UIBarButtonItem!
     
+    @IBOutlet weak var keyboardHeightLayoutConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var recordingImage: UIImageView!
     
@@ -59,6 +60,34 @@ class RecorderViewController: UIViewController, AVAudioRecorderDelegate, UITextF
         descriptionTxt.delegate = self
         
         buttonsOnLoad()
+        
+        // Note that SO highlighting makes the new selector syntax (#selector()) look
+        // like a comment but it isn't one
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardNotification(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillChangeFrame,
+                                               object: nil)
+    }
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame!.origin.y ?? 0
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrameY >= UIScreen.main.bounds.size.height {
+                self.keyboardHeightLayoutConstraint?.constant = 154.0
+            } else {
+                self.keyboardHeightLayoutConstraint?.constant = endFrame?.size.height ?? 154.0
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
     }
     
     @IBAction func feedNavBtn(_ sender: UIBarButtonItem) {
